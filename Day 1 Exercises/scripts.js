261bac13affd5bec1897200348506310776cdda7
ZakkyAi <zakkyalwafa@gmail.com>
2024-04-07 19:45:18 +0700

    first commit

--- Day 1 Exercises/scripts.js
+++ Day 1 Exercises/scripts.js
@@ -1,3 +1,25 @@
 console.log('comments can make code readable')
 console.log('Welcome to 30DaysOfJavaScript')
 console.log('comments can make code readable', 'easy to reuse and informative')
+
+
+let variable1
+let variable2
+let variable3
+let variable4
+
+let variable5 = "wajojfe"
+let variable6 = 80
+let variable7 = null
+let variable8 = false
+
+let nama = 'zaki'
+let lastname = 'wafa'
+let maritalStatus = false
+let country = 'palembang'
+let age = 19
+
+let nama1 = 'zaki', lastname1 = 'wafa', maritalStatus1 = false, country1 = 'palembang', age1 = 19;
+
+let myage = 25, yourage = 30
+console.log('I am', myage, 'years old.' + '\n' + 'Your are',  yourage, 'years old.')
---
+++ Day 1 Exercises/variable.js
@@ -0,0 +1,11 @@
+let firstName = 'Asabeneh'
+console.log(firstName)
+
+let isMarried = true;
+console.log(isMarried)
+
+let undiVariable
+console.log(undiVariable)
+
+let emptyValue = null
+console.log(emptyValue)
