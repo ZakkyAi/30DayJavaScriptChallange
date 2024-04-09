46f1c4ef407d4347ee0ba8d53c4051af98a5ab4c
ZakkyAi <zakkyalwafa@gmail.com>
2024-04-09 23:48:05 +0700

    day 3

---
+++ Day 3 Exercises/script.js
@@ -0,0 +1,66 @@
+// let firstName = 'zaki'
+// let lastName = 'wafa'
+// let country = 'indonesia'
+// let city = 'palembang'
+// let age = 19
+// let isMarried = false
+
+// console.log(typeof firstName, typeof lastName, typeof country, typeof city, typeof age, typeof isMarried)
+
+// let exam = '10'
+// console.log(exam === 10)
+
+// let exam = '9.81'
+// let numInt = Math.round(parseFloat(exam))
+// console.log(exam === 10)
+
+// let exam1 = 2 > 1
+// let exam2 = 3/3 == 1
+// let exam3 = 4 * 1 == 4
+// console.log(exam1)
+// console.log(exam2)
+// console.log(exam3)
+
+// let exam1 = 2 > 4
+// let exam2 = 3/3 == 4
+// let exam3 = 4 * 1 == 6
+// console.log(exam1)
+// console.log(exam2)
+// console.log(exam3)
+
+// console.log(!(4 === '4'))
+
+// let kaleng = 'phyton'
+// let jargon = 'jargon'
+// console.log(kaleng.length !== jargon.length)
+
+// let kaleng = 'python';
+// let jargon = 'jargon';
+// console.log(kaleng.match(/on/gi), jargon.match(/on/gi));
+
+// const yearnow = new Date()
+// console.log(yearnow.getFullYear())
+
+// const yearnow = new Date()
+// console.log(yearnow.getTime())
+
+// let base = prompt('masukan base')
+// let height = prompt('masukan height')
+// alert('The area of the triangle is '+ base * height * 0.5)
+
+// let input1 = parseFloat(prompt('enter side a'))
+// let input2 = parseFloat(prompt('enter side b'))
+// let input3 = parseFloat(prompt('enter side c'))
+// let total = input1 + input2 + input3
+
+// alert('The perimeter of the triangle is ' + total)
+
+// let pi = 3.14
+// let r = parseFloat(prompt('enter side b'))
+// let luas = r * pi * r
+
+// let keliling = 2 * pi * r
+
+// alert('keliling ' + keliling)
+// alert('luas ' + luas)
+
