900dbec044504e0c668f2fc3fa6d90d5c181cd5e
ZakkyAi <zakkyalwafa@gmail.com>
2024-04-16 23:54:04 +0700

    day 10

---
+++ Day 10 Exercises/script.js
@@ -0,0 +1,17 @@
+const languages = [
+    'English',
+    'Finnish',
+    'English',
+    'French',
+    'Spanish',
+    'English',
+    'French',
+  ]
+  
+  const setOfLanguages = new Set(languages)
+  
+  for (const language of setOfLanguages) {
+    console.log(language)
+  }
+
+  Set(4) {"English", "Finnish", "French", "Spanish"}
